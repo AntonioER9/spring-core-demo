a978a96e6ef9f19c3c6622e6f03598cb8118746f
Antonio Espinoza <a0e0bhj@walmart.com>
2025-05-30 12:12:27 -0400

    first commit

---
+++ src/main/java/com/antonio/springcoredemo/Coach.java
@@ -0,0 +1,6 @@
+package com.antonio.springcoredemo;
+
+public interface Coach {
+
+    String getDailyWorkout();
+}
---
+++ src/main/java/com/antonio/springcoredemo/CricketCoach.java
@@ -0,0 +1,13 @@
+package com.antonio.springcoredemo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CricketCoach implements Coach {
+
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice for Cricket Coach";
+    }
+}
