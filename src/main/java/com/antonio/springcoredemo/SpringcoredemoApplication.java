1752c596a48784a0e5c71b80adf1ceee3feec98b
Antonio Espinoza <a0e0bhj@walmart.com>
2025-05-30 12:49:01 -0400

    Update SpringBootApplication annotation to specify base packages

--- src/main/java/com/antonio/springcoredemo/SpringcoredemoApplication.java
+++ src/main/java/com/antonio/springcoredemo/SpringcoredemoApplication.java
@@ -3,7 +3,9 @@ package com.antonio.springcoredemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+		scanBasePackages = {"com.antonio.springcoredemo","com.antonio.util"}
+)
 public class SpringcoredemoApplication {
 
 	public static void main(String[] args) {
